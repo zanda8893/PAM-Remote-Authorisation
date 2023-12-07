c5e7a3935678985add9d9d1bd56d455cc4d2638e
Alexander Hallard <zanda.hallard@outlook.com>
2023-12-07 09:05:36 +0000

    Got discord approval working

--- discord_bot.py
+++ discord_bot.py
@@ -7,7 +7,7 @@ import main
 
 import discord
 from discord import Intents
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.utils import get
 from dotenv import load_dotenv
 
@@ -20,6 +20,7 @@ if getenv('DEBUG'):
 intents = Intents.default()
 intents.members = True
 intents.message_content = True
+intents.reactions = True
 
 bot = discord.Bot(command_prefix="!", intents=intents)
 
@@ -32,14 +33,20 @@ async def on_ready():  # When bot is ready
 
     bot.channel = get(bot.guild.channels, name="approval")
     await bot.channel.send("Bot online")
+    #bot.loop.create_task(test_task())
     bot.loop.create_task(check_events())
+    bot.approvals = []
 
+async def test_task():
+    while True:  # Replace this with your task's condition
+        print("Bluh")
+        await asyncio.sleep(60)  # For example, wait for 60 seconds
 
 async def check_events():
     while True:
         event_data = await main.retrieve_event()
         if event_data:
-            await approve()
+            await approve(event_data)
         await asyncio.sleep(5)
 
 
@@ -51,8 +58,21 @@ async def status(ctx):  # (TODO: add more to status)
     await ctx.respond(f"Working")
 
 
-async def approve():
-    await bot.channel.send("Called from main")
+async def approve(data):
+    data['message'] = await bot.channel.send(f"{data['user']} is attempting to run {data['cmd']} approve?")
+    bot.approvals.append(data)
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    message = reaction.message  # our embed
+    if message.channel.id == bot.channel.id:  # checking if it's the same channel
+        if message.author == bot.user:  # checking if it's sent by the bot
+            if str(reaction.emoji) == "👍":  # checking the emoji
+                for approve in bot.approvals:
+                    if approve['message'].id == message.id:
+                        approve['approve'] = 1
+                        print(bot.approvals)
 
 bot.run(getenv('DISCORD_TOKEN'))
 
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from os import getenv
 from time import sleep
 import asyncio
+import aiofiles
 
 from dotenv import load_dotenv
-
-import discord_bot
 
 load_dotenv()
 
 
 async def retrieve_event():
+    data_dict = {
+        "user": "",
+        "cmd": "",
+        "approve": 0,
+        "message": None
+    }
     # Simulating retrieving an event
-    with open("/tmp/py-c") as pipe:
-        data = pipe.read()
-        print(data)
+    async with aiofiles.open('/tmp/py-c', mode='r') as pipe:
+        data = await pipe.read()
+        data = data.split(" ")
+        data_dict["user"] = data[0]
+        data_dict["cmd"] = data[1]
+        return data_dict
+
+
+if __name__ == "__main__":
+    test = asyncio.run(retrieve_event())
+    print(test)
